b689be20924d7740d73906ad3a83455ac6ef28c4
BojOnTheBeat <bjfatade@gmail.com>
2020-06-13 20:27:09 -0400

    add functions to talk to mongo

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("localhost") // TODO: make this configurable
+	return err
+}
+
+func closeddb() {
+	db.Close()
+	log.Println("closed database connection")
+}
+
 func main() {
 
 }
