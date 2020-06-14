5478980f350d669d345b11baf07abf671e6667eb
BojOnTheBeat <bjfatade@gmail.com>
2020-06-14 15:18:58 -0400

    implement function to start twitter stream

--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -147,3 +147,27 @@ func readFromTwitter(votes chan<- string) {
 		}
 	}
 }
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+	stoppedchan := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+		}()
+		for {
+			select {
+			case <-stopchan:
+				log.Println("stopping Twitter...")
+				return
+
+			default:
+				log.Println("Queryying Twitter")
+				readFromTwitter(votes)
+				log.Println(" (waiting)")
+				time.Sleep(10 * time.Second) // wait before reconnecting
+			}
+		}
+
+	}()
+	return stoppedchan
+}
